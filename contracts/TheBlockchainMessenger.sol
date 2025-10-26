c36127ac428980927c72de7ea8bf3144a28174cb
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2025-10-26 09:30:44 +0100

    added the blockchain messager file

---
+++ contracts/TheBlockchainMessenger.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.30;
+
+
+contract TheBlockchainMessenger {
+    uint public changeCounter;
+
+    address public owner;
+
+    string public theMessage;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function updateTheMessage(string memory _newMessage) public {
+        if(msg.sender == owner) {
+            theMessage = _newMessage;
+            changeCounter++;
+        }
+    }
+}
